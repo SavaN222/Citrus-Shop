f4426db21eaf28689780f39323916e51b4a42089
SavaN222 <nakaradasava@gmail.com>
2020-03-04 21:52:57 +0100

    Finish Products design 3x3

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -3,14 +3,95 @@
     <h2 class="text-dark text-center">Our Products</h2>
     <div class="row">
         <div class="col-lg-4">
-            
+           <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/oranges.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Oranges</h5>
+                <p class="card-text">Super Cool Oranges.</p>
+              </div>
+            </div> 
         </div>
+
         <div class="col-lg-4">
-            
+            <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/lemons.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Lemons</h5>
+                <p class="card-text">Super Cool Lemons.</p>
+              </div>
+            </div> 
         </div>
+
         <div class="col-lg-4">
-            
+            <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/kiwi.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Kiwi</h5>
+                <p class="card-text">Super Cool Kiwi.</p>
+              </div>
+            </div> 
         </div>
+
+         <div class="col-lg-4">
+           <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/oranges.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Oranges</h5>
+                <p class="card-text">Super Cool Oranges.</p>
+              </div>
+            </div> 
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/lemons.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Lemons</h5>
+                <p class="card-text">Super Cool Lemons.</p>
+              </div>
+            </div> 
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/kiwi.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Kiwi</h5>
+                <p class="card-text">Super Cool Kiwi.</p>
+              </div>
+            </div> 
+        </div>
+
+         <div class="col-lg-4">
+           <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/oranges.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Oranges</h5>
+                <p class="card-text">Super Cool Oranges.</p>
+              </div>
+            </div> 
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/lemons.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Lemons</h5>
+                <p class="card-text">Super Cool Lemons.</p>
+              </div>
+            </div> 
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card">
+              <img src="<?php echo URLROOT; ?>/images/products/kiwi.jpg" class="card-img-top w-100" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Kiwi</h5>
+                <p class="card-text">Super Cool Kiwi.</p>
+              </div>
+            </div> 
+        </div>
+
     </div>
 </div> <!-- container -->
 <?php require APPROOT . '/views/inc/footer.php'; ?>
